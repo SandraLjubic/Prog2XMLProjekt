fc441a0637f611ee94bbcd00adf43d1cb29de354
d.mitchell <d.mitchell@DESKTOP-M92TTAL.fritz.box>
2020-06-17 19:07:50 +0200

    remove löhne, stoffe, betriebskosten, gerätekosten, sonstiges, ep und gp

--- Prog2XMLProjekt/src/Anwendung/LineDTO.java
+++ Prog2XMLProjekt/src/Anwendung/LineDTO.java
@@ -2,45 +2,28 @@ package Anwendung;
 
 /*
  * Data Transfer Object
+ * 
  */
 public class LineDTO {
 
 	private String oz;
-	private String posArt;
 	private String kurztext;
 	private String menge;
 	private String einheit;
-	private String loehne;
-	private String stoffe;
-	private String betriebskosten;
-	private String geraetekosten;
-	private String sonstiges;
-	private String nachunternehmer;
-	private String ep;
-	private String gp;
 
-	public LineDTO(String oz, String posArt, String kurztext, String menge, String einheit, String loehne,
-			String stoffe, String betriebskosten, String geraetekosten, String sonstiges, String nachunternehmer,
-			String ep, String gp) {
+
+	public LineDTO(String oz, String kurztext, String menge, String einheit) {
 		super();
 		this.oz = oz;
-		this.posArt = posArt;
 		this.kurztext = kurztext;
 		this.menge = menge;
 		this.einheit = einheit;
-		this.loehne = loehne;
-		this.stoffe = stoffe;
-		this.betriebskosten = betriebskosten;
-		this.geraetekosten = geraetekosten;
-		this.sonstiges = sonstiges;
-		this.nachunternehmer = nachunternehmer;
-		this.ep = ep;
-		this.gp = gp;
+
 	}
 
 	@Override
 	public String toString() {
-		return oz + " | " + posArt + " | " + kurztext + " | " + menge + " | " + einheit;
+		return oz + " " + kurztext + " " + menge + " " + einheit;
 	}
 
 	public String getOz() {
@@ -51,13 +34,6 @@ public class LineDTO {
 		this.oz = oz;
 	}
 
-	public String getPosArt() {
-		return posArt;
-	}
-
-	public void setPosArt(String posArt) {
-		this.posArt = posArt;
-	}
 
 	public String getKurztext() {
 		return kurztext;
@@ -81,70 +57,6 @@ public class LineDTO {
 
 	public void setEinheit(String einheit) {
 		this.einheit = einheit;
-	}
-
-	public String getLoehne() {
-		return loehne;
-	}
-
-	public void setLoehne(String loehne) {
-		this.loehne = loehne;
-	}
-
-	public String getStoffe() {
-		return stoffe;
-	}
-
-	public void setStoffe(String stoffe) {
-		this.stoffe = stoffe;
-	}
-
-	public String getBetriebskosten() {
-		return betriebskosten;
-	}
-
-	public void setBetriebskosten(String betriebskosten) {
-		this.betriebskosten = betriebskosten;
-	}
-
-	public String getGeraetekosten() {
-		return geraetekosten;
-	}
-
-	public void setGeraetekosten(String geraetekosten) {
-		this.geraetekosten = geraetekosten;
-	}
-
-	public String getSonstiges() {
-		return sonstiges;
-	}
-
-	public void setSonstiges(String sonstiges) {
-		this.sonstiges = sonstiges;
-	}
-
-	public String getNachunternehmer() {
-		return nachunternehmer;
-	}
-
-	public void setNachunternehmer(String nachunternehmer) {
-		this.nachunternehmer = nachunternehmer;
-	}
-
-	public String getEp() {
-		return ep;
-	}
-
-	public void setEp(String ep) {
-		this.ep = ep;
-	}
-
-	public String getGp() {
-		return gp;
-	}
-
-	public void setGp(String gp) {
-		this.gp = gp;
 	}
 
 }
--- Prog2XMLProjekt/src/Anwendung/Main.java
+++ Prog2XMLProjekt/src/Anwendung/Main.java
@@ -5,27 +5,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.management.RuntimeErrorException;
-import javax.sound.sampled.Line;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.xml.parsers.DocumentBuilder;
-
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
-
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
@@ -51,20 +43,20 @@ public class Main {
 
 			@Override
 			public int compare(LineDTO o1, LineDTO o2) {
-				
+
 				String a = o1.getOz().replaceAll("\\.", "");
-				String b = o2.getOz().replaceAll("\\.","");
-				
-				while(a.length()<3) {
+				String b = o2.getOz().replaceAll("\\.", "");
+
+				while (a.length() < 3) {
 					a = a + "0";
 				}
-				while(b.length()<3) {
-					b = b+ "0";
+				while (b.length() < 3) {
+					b = b + "0";
 				}
-				
+
 				Integer aInt = Integer.parseInt(a);
-				Integer bInt= Integer.parseInt(b);
-				
+				Integer bInt = Integer.parseInt(b);
+
 				return aInt.compareTo(bInt);
 			}
 
@@ -102,9 +94,7 @@ public class Main {
 
 	private static LineDTO parseDTOFirstLvl(Node item, int ebene1) throws XPathExpressionException {
 		String oz = item.getAttributes().getNamedItem("RNoPart").getNodeValue();
-		String posArt = "BEREICH";
-		String kurztext = null;
-		String gp = null;
+		String kurztext = "";
 
 		NodeList secondLvl = ((Node) xpath.compile("*/Award/BoQ/BoQBody/BoQCtgy[" + ebene1 + "]/BoQBody").evaluate(doc,
 				XPathConstants.NODE)).getChildNodes();
@@ -112,15 +102,13 @@ public class Main {
 			list.add(parseDTOSecondLvl(secondLvl.item(i), oz + ".", ebene1, i + 1));
 		}
 
-		return new LineDTO(oz, posArt, kurztext, null, null, null, null, null, null, null, null, null, gp);
+		return new LineDTO(oz, kurztext, "", "");
 	}
 
 	private static LineDTO parseDTOSecondLvl(Node item, String preOz, int ebene1, int ebene2)
 			throws XPathExpressionException {
 		String oz = preOz + item.getAttributes().getNamedItem("RNoPart").getNodeValue();
-		String posArt = "BEREICH";
 		String kurztext = getKurztextEbene2(item, ebene1, ebene2);
-		String gp = null;
 
 		NodeList thridLvl = ((Node) xpath
 				.compile("*/Award/BoQ/BoQBody/BoQCtgy[" + ebene1 + "]/BoQBody/BoQCtgy[" + ebene2 + "]/BoQBody/Itemlist")
@@ -141,7 +129,7 @@ public class Main {
 			list.add(parseDTOEinheit(oz + ".", ebene1, ebene2, i + 1 + korrekturIndex));
 		}
 		korrekturIndex = 0;
-		return new LineDTO(oz, posArt, kurztext, null, null, null, null, null, null, null, null, null, gp);
+		return new LineDTO(oz, kurztext, "", "");
 	}
 
 	private static LineDTO parseDTOEinheit(String preOz, int ebene1, int ebene2, int ebene3)
@@ -151,20 +139,11 @@ public class Main {
 				+ "]/BoQBody/Itemlist/Item[" + ebene3 + "]").evaluate(doc, XPathConstants.NODE));
 
 		String oz = preOz + item.getAttributes().getNamedItem("RNoPart").getNodeValue();
-		String posArt = "EPANTEILE";
 		String kurztext = getKurztextEbene3(item);
 		String menge = getMenge(item);
 		String einheit = getEinheit(item);
-		String loehne = null;
-		String stoffe = null;
-		String betriebskosten = null;
-		String geraetekosten = null;
-		String sonstiges = null;
-		String nachunternehmer = null;
-		String ep = null;
-		String gp = null;
-		return new LineDTO(oz, posArt, kurztext, menge, einheit, loehne, stoffe, betriebskosten, geraetekosten,
-				sonstiges, nachunternehmer, ep, gp);
+
+		return new LineDTO(oz, kurztext, menge, einheit);
 	}
 
 	private static String getEinheit(Node item) throws XPathExpressionException {
