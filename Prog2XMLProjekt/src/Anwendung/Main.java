6e12e61d8d2fcf418960b5485e5e9eb4b29f7258
elena <elena@air-elena.fritz.box>
2020-06-24 15:25:33 +0200

    GUI fertig

--- Prog2XMLProjekt/src/Anwendung/Main.java
+++ Prog2XMLProjekt/src/Anwendung/Main.java
@@ -1,11 +1,11 @@
 package Anwendung;
 
-import GUI.GUI2;
+import GUI.GUI;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new GUI2();
+		new GUI();
 
 	}
 
--- Prog2XMLProjekt/src/GUI/GUI.java
+++ Prog2XMLProjekt/src/GUI/GUI.java
@@ -1,26 +1,24 @@
 package GUI;
 
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Arrays;
-
 import javax.swing.*;
 
-public class GUI2 implements ActionListener {
+public class GUI implements ActionListener {
 	public JFrame frame;
 
 	static String[][] arrayFuerGui;
 
-	String ueberschrift[] = { "OZ", "Menge", "Einh.", "Kurztext" };
+	static String ueberschrift[] = { "OZ", "Menge", "Einh.", "Kurztext" };
 
 	public File file;
 
-	public GUI2() {
-		frame = new JFrame();
+	public GUI() {
 
 		// Frame data
+
+		frame = new JFrame();
 		frame.setTitle("GAEB X-83 Data Opener");
 		frame.setBounds(0, 0, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,6 +29,7 @@ public class GUI2 implements ActionListener {
 		JLabel name = new JLabel("<html><span style='font-size:18px'>GAEB X-83 Data Opener</span></html>");
 
 		// Menu data
+
 		JMenuBar mb = new JMenuBar();
 		JMenu info = new JMenu("Programm");
 		JMenuItem about = new JMenuItem("About");
@@ -40,8 +39,9 @@ public class GUI2 implements ActionListener {
 		mb.add(info);
 
 		// Action Listeners
+
 		about.addActionListener(new ActionListener() {
-			@Override
+
 			public void actionPerformed(ActionEvent actionEvent) {
 				JOptionPane.showMessageDialog(null, "Developerteam:\nKamila Sultanova, s0564709;"
 						+ "\nSandra Ljubic, s0555006;" + "\nDavid O. Mitchell, s0570705.");
@@ -49,7 +49,6 @@ public class GUI2 implements ActionListener {
 		});
 
 		menuExit.addActionListener(new ActionListener() {
-			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
 				System.exit(0);
 			}
@@ -65,11 +64,14 @@ public class GUI2 implements ActionListener {
 		exit.addActionListener(this);
 		exit.setActionCommand("exitProgramm");
 
+		// Setting bounds
+
 		mb.setBounds(0, 0, 800, 25);
 		name.setBounds(250, 30, 400, 50);
-
 		fileChooser.setBounds(20, 510, 125, 50);
 		exit.setBounds(655, 510, 125, 50);
+
+		// Adding elements to frame
 
 		frame.add(mb);
 		frame.add(name);
@@ -81,37 +83,31 @@ public class GUI2 implements ActionListener {
 
 	public void OpenFile() {
 		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setCurrentDirectory(new File("/Users/elena/eclipse-workspace/final"));
+		fileChooser.setCurrentDirectory(new File("/Users/elena/"));
 		int result = fileChooser.showOpenDialog(new JFrame());
 		if (result == JFileChooser.APPROVE_OPTION) {
 			File selectedFile = fileChooser.getSelectedFile();
-			System.out.println(selectedFile.getAbsolutePath());
+//			System.out.println(selectedFile.getAbsolutePath());
 			this.file = new File(selectedFile.getAbsolutePath());
 			arrayFuerGui = ReadXML.xml(this.file);
-			for (String[] i : arrayFuerGui) {
-				System.out.println(Arrays.toString(i));
-			}
+//			for (String[] i : arrayFuerGui) {
+//				System.out.println(Arrays.toString(i));
+//			}
 			AddTable();
 
 		}
 	}
 
-	// TODO:
 	public void AddTable() {
 
-		JTable table = new JTable(arrayFuerGui, ueberschrift);
-		table.setBounds(50, 120, 700, 350);
-		JScrollPane sp = new JScrollPane(table);
+		JTable x = new JTable(arrayFuerGui, ueberschrift);
+		JScrollPane sp = new JScrollPane(x);
+		sp.setBounds(10, 120, 780, 370);
 
-		// sp.revalidate();
-		// table.getTableHeader().setPreferredSize(new Dimension(table.getSize().width,
-		// 40));
-
-		frame.add(table);
+		frame.add(sp);
 
 	}
 
-	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("fileChooser")) {
 			OpenFile();
